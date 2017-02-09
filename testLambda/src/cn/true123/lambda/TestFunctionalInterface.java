3631fef1b0d5ddb8da450ca88e8e2e5623b64c71
feimeng0530 <feimeng0530@163.com>
2017-02-09 22:44:18 +0800

    add predicate test

--- testLambda/src/cn/true123/lambda/TestFunctionalInterface.java
+++ testLambda/src/cn/true123/lambda/TestFunctionalInterface.java
@@ -1,8 +1,14 @@
 package cn.true123.lambda;
 
+import java.util.function.Predicate;
+
 public class TestFunctionalInterface {
 	public static void main(String[] args) {
 		FunctionalInterface functionalInterface = () -> System.out.println("Hi Functional interface!");
 		functionalInterface.test();
+
+		Predicate<String> perdicate = s -> s.equals("test");
+		System.out.println(perdicate.test("test"));
+		System.out.println(perdicate.test("test false"));
 	}
 }
